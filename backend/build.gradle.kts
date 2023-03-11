34e1fdf8bd50555662204f7fba95a80af7c83ac8
jmazor <mazorj@pm.me>
2023-03-11 03:38:55 -0500

    Project setup

---
+++ backend/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    java
+    war
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+group = "vudb.me"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
