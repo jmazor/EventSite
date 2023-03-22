711733684420d3355bc0529e33847af56646d936
jmazor <mazorj@pm.me>
2023-03-22 01:16:41 -0400

    jenkins

--- backend/src/main/java/me/vudb/backend/controller/EventController.java
+++ backend/src/main/java/me/vudb/backend/controller/EventController.java
@@ -2,7 +2,11 @@ package me.vudb.backend.controller;
 
 import me.vudb.backend.event.EventService;
 import me.vudb.backend.security.JwtTokenUtil;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +24,15 @@ public class EventController {
         this.jwtTokenUtil = jwtTokenUtil;
     }
 
+    @PostMapping("/create/public")
+    public ResponseEntity<?> createPublicEvent() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        // TODO: ensure that the user is a admin
+        String username = auth.getName();
+        
 
+        return null;
+    }
 
 
 }
